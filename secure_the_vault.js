dea7f4570b73e69c7ddb11395f778a20a7963a48
Jhbalza2002 <166169089+Jhbalza2002@users.noreply.github.com>
2024-05-23 15:26:40 -0500

    Add files via upload

---
+++ secure_the_vault.js
@@ -0,0 +1,15 @@
+//Pseudocode
+//Make a message variable
+// add unique calculation to each variable
+//create an alert message
+//link index.html file to secure_the_vault.js
+
+//This is a Pop Up message
+let message =
+  "Yo have recieve a message, you have been chosen tom open an important vault. Here is the secret combination";
+//these numbers equals the combination for the secret vault
+let num_1 = 2 * 5;
+let num_2 = 20 * 2;
+let num_3 = 38 + 1;
+//This create the alert for the user
+alert(`${message} ${num_1} ${num_2} ${num_3}`);
